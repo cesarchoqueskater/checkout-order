f94dca062829623a66803cbefa2bdbebbb2506fa
cchoqueh <cesarchoqueskater@gmail.com>
2021-11-16 02:04:18 -0500

    Logica para agregar y sustraer cantidad del Producto

--- js/index.js
+++ js/index.js
@@ -3,9 +3,15 @@
 
 const $optionSelectedInformationProcessor = document.querySelectorAll('.information-processor div')
 const $optionSelectedInformationStorage = document.querySelectorAll('.information-storage div')
+const $btnAddProduct = document.querySelector('.btn-add')
+const $btnRemoveProduct = document.querySelector('.btn-minus')
+const $valueCountProduct = document.querySelector('.count-product-value')
+
 
 selectInformationProcessor()
 selectInformationStorage()
+addCountProduct()
+substractCountProduct()
 
 export function removeAttributeInformationProcessor() {
     $optionSelectedInformationProcessor.forEach((item) => {
@@ -41,5 +47,27 @@ export function selectInformationStorage() {
             removeAttributeInformationStorage()
             item.setAttribute('aria-selected', true)
         })
+    })
+}
+
+export function addCountProduct() {
+    $btnAddProduct.addEventListener('click', function() {
+        console.log("Se hizo click en boton para agregar +1")
+        let countProduct = Number($valueCountProduct.textContent) + 1
+        $valueCountProduct.innerText = countProduct
+    })
+}
+
+export function substractCountProduct() {
+    $btnRemoveProduct.addEventListener('click', function() {
+        console.log("Se hizo click en boton para restar -1")
+        let countProduct;
+        if (Number($valueCountProduct.textContent) == 0) {
+            Number($valueCountProduct.textContent)
+        } else {
+            let countProduct = Number($valueCountProduct.textContent) - 1
+            $valueCountProduct.innerText = countProduct
+        }
+
     })
 }
